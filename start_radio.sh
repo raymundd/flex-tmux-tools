8bbaa8c66a8d487c1434e2b06f0d41ebddea089f
raymundd <rayd.9100@gmail.com>
2021-07-17 14:50:07 +0100

    Allow multicase STATION name and try to catch already active tmux session

--- start_radio.sh
+++ start_radio.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
+#Locations of programs
 DAX_PROG=nDAX-linux-amd64
 CAT_PROG=nCAT-linux-amd64
 DAX_DIR=/home/$USER/Flexradio
-PROG=${DAX_PROG:0:15}
+WSJTX=/usr/local/bin/wsjtx
 
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <radio_name>"
+    echo "Usage: $0 <STATION>"
     echo "Current Radio options:"
-    echo "bluemoon, reddwarf"
+    echo "BLUEMOON, REDDWARF"
+    echo "Scripts are case sensitive so make sure you enter the station name exactly as displayed in SmartSDR."
     exit
 fi
 
+#Some example configs
 if [ ${1^^} == "BLUEMOON" ]; then
     PORT=64001
+    CAT_PORT=:4532
     STATION=BLUEMOON
     RADIO=192.168.42.144
 elif [ ${1^^} == "REDDWARF" ] ; then
     PORT=64002
+    CAT_PORT=:4533
     STATION=REDDWARF
     RADIO=192.168.42.119
 else
@@ -25,10 +30,17 @@ else
     exit
 fi
 
+#Make sure these are executable
 chmod +x $DAX_DIR/$DAX_PROG
+chmod +x $DAX_DIR/$CAT_PROG
+
+if [ $(tmux ls | grep -c "${STATION^^}") -gt 0 ]; then
+    echo "Session may already be running - abandoning run."
+    tmux ls
+    exit
+fi
 
 #Start nDAX, nCAT and WSJT-X
-STATION=${STATION^^}
-tmux new-session -d -s $STATION -n "$STATION-DAX" "$DAX_DIR/$DAX_PROG -station $STATION -udp-port $PORT -radio $RADIO -source $STATION.rx -sink $STATION.tx"
-tmux new-window -d -n "$STATION-CAT" "$DAX_DIR/$CAT_PROG -station $STATION -listen :4532 -radio $RADIO"
-tmux new-window -d -n "$STATION-WSJTX" "wsjtx -r $STATION"
+tmux new-session -d -s ${STATION^^} -n "${STATION^^}-DAX" "$DAX_DIR/$DAX_PROG -station $STATION -udp-port $PORT -radio $RADIO -source $STATION.rx -sink $STATION.tx"
+tmux new-window -d -n "${STATION^^}-CAT" "$DAX_DIR/$CAT_PROG -station $STATION -listen $CAT_PORT -radio $RADIO"
+tmux new-window -d -n "${STATION^^}-WSJTX" "$WSJTX -r $STATION"
