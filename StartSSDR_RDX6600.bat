7fe7fa230e9542b272c3ed6fd52ea01545365ad9
raymundd <rayd.9100@gmail.com>
2021-07-25 13:51:23 +0100

    Wrong STATION set.

--- StartSSDR_RDX6600.bat
+++ StartSSDR_RDX6600.bat
@@ -2,5 +2,5 @@
 @REM This is just so I can control the initial STATION name.
 
 copy /Y "%APPDATA%\FlexRadio Systems\SSDR.settings" "%APPDATA%\FlexRadio Systems\SSDR_Orig.settings"
-copy /Y "%APPDATA%\FlexRadio Systems\SSDR_RDX6500.settings" "%APPDATA%\FlexRadio Systems\SSDR.settings"
+copy /Y "%APPDATA%\FlexRadio Systems\SSDR_RDX6600.settings" "%APPDATA%\FlexRadio Systems\SSDR.settings"
 start "RDX6600" "%PROGRAMFILES%\FlexRadio Systems\SmartSDR v3.2.39\SmartSDR.exe"
