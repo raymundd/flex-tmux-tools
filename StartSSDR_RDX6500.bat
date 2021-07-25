10d0612690c64482380216de759cc4d94fab7eb4
raymundd <rayd.9100@gmail.com>
2021-07-25 13:27:46 +0100

    Force copy to complete without prompt

--- StartSSDR_RDX6500.bat
+++ StartSSDR_RDX6500.bat
@@ -1,6 +1,6 @@
 @REM Start an instance of SmartSDR using a pre-determined settings file
 @REM This is just so I can control the initial STATION name.
 
-copy "%APPDATA%\FlexRadio Systems\SSDR.settings" "%APPDATA%\FlexRadio Systems\SSDR_Orig.settings"
-copy "%APPDATA%\FlexRadio Systems\SSDR_RDX6500.settings" "%APPDATA%\FlexRadio Systems\SSDR.settings"
+copy /Y "%APPDATA%\FlexRadio Systems\SSDR.settings" "%APPDATA%\FlexRadio Systems\SSDR_Orig.settings"
+copy /Y "%APPDATA%\FlexRadio Systems\SSDR_RDX6500.settings" "%APPDATA%\FlexRadio Systems\SSDR.settings"
 start "RDX6500" "%PROGRAMFILES%\FlexRadio Systems\SmartSDR v2.7.6\SmartSDR.exe"
--- StartSSDR_RDX6600.bat
+++ StartSSDR_RDX6600.bat
@@ -1,6 +1,6 @@
 @REM Start an instance of SmartSDR using a pre-determined settings file
 @REM This is just so I can control the initial STATION name.
 
-copy "%APPDATA%\FlexRadio Systems\SSDR.settings" "%APPDATA%\FlexRadio Systems\SSDR_Orig.settings"
-copy "%APPDATA%\FlexRadio Systems\SSDR_RDX6500.settings" "%APPDATA%\FlexRadio Systems\SSDR.settings"
+copy /Y "%APPDATA%\FlexRadio Systems\SSDR.settings" "%APPDATA%\FlexRadio Systems\SSDR_Orig.settings"
+copy /Y "%APPDATA%\FlexRadio Systems\SSDR_RDX6500.settings" "%APPDATA%\FlexRadio Systems\SSDR.settings"
 start "RDX6600" "%PROGRAMFILES%\FlexRadio Systems\SmartSDR v3.2.39\SmartSDR.exe"
