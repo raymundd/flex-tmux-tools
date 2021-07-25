a045c93d2c68575f8cfc815fd3e4272c83b41a01
raymundd <rayd.9100@gmail.com>
2021-07-25 11:08:13 +0100

    Add some cleanup routines.

--- stop_radio.sh
+++ stop_radio.sh
@@ -4,11 +4,11 @@ if [ $# -ne 1 ] ; then
     echo "Usage: $0 <station_name>"
 fi
 
+#Send CTRL-C to all panes.
 tmux list-panes -st ${1^^} -F '#{session_name}:#{window_index}' | xargs -I WINDOW tmux send-keys -t WINDOW C-c
 
 #Cleanup for failed startup
 sleep 5
 #Need to only kill the following if they are related to the session's STATION name
-#pkill nDAX
-#pkill nCAT
-#pkill pulseaudio
+pkill -f "^[^tmux].*nDAX.*${1}"
+pkill -f "^[^tmux].*nCAT.*${1}"
