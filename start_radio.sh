053ba0ff523be7f88c8f64f12ecfee1d24432431
raymundd <rayd.9100@gmail.com>
2021-08-07 14:29:40 +0100

    Using a small launch script to get the process pid from within tmux.

--- start_radio.sh
+++ start_radio.sh
@@ -56,29 +56,39 @@ if [ $(tmux ls | grep -c "${STATION^^}") -gt 0 ]; then
     exit
 fi
 
+# Create a launch script
+cat <<EOF > /tmp/wsjtx.sh
+#!/bin/bash
+${WSJTX} -r ${STATION}&
+PROC1=\$!
+echo \$PROC1 > /tmp/${STATION}.pid
+wait \$PROC1
+echo "WSJT - FINISHED"
+EOF
+
+# Prepare the launch script and pid file
+chmod +x /tmp/wsjtx.sh
+rm /tmp/${STATION}.pid
+
 #Start nDAX, nCAT and WSJT-X
 tmux start-server
 tmux new-session -s ${STATION^^} -d -n "${STATION^^}-DAX" "$DAX_DIR/$DAX_PROG -station $STATION -udp-port $PORT -radio $RADIO -source $STATION.rx -sink $STATION.tx"
-# Lets remember the pid of this tmux session so that we can find the associated instance of WSJTX.exe that was launched.
-PROC=$!
-
 tmux new-window -d -n "${STATION^^}-CAT" "$DAX_DIR/$CAT_PROG -station $STATION -listen $CAT_PORT -radio $RADIO"
-tmux new-window -d -n "${STATION^^}-WSJTX" "$WSJTX -r $STATION"
+tmux new-window -d -n "${STATION^^}-WSJTX" /tmp/wsjtx.sh
 
-# Using the BASH sessions PID locate the PID of SmartSDR.exe, or bail.
-for pid in "$(pgrep --parent $PROC)"
-do
-	echo $pid
-	if [[ -n $pid ]] && [[ $(ps --no-headers -fp $pid | egrep -c ".+wsjtx.exe") -eq 1 ]]
-	then
-		WSJTX_P=$pid
-		break
-	else
-		echo "ERROR: WSJTX.exe not running."
-		echo
-		exit 1
-	fi
+# Lets remember the pid of this tmux session so that we can find the associated instance of WSJTX.exe that was launched.
+# Wait for the pid file to be created.
+loop=0
+while [ ! -e /tmp/${STATION}.pid ]; do
+        sleep 1
+        ((loop++))
+        if [ $loop -gt 10 ]; then
+                echo "ERROR - Process has failed."
+        fi
 done
+
+WSJTX_P=$(cat /tmp/${STATION}.pid)
+echo $WSJTX_P
 
 # Spinner routine - Lets just show that the script is alive and waiting for SmartSDR.exe to exit.
 spin='-\|/'
@@ -92,3 +102,7 @@ do
 done
 
 echo
+
+#TODO - Clear down the tmux session for this instance.
+rm /tmp/wsjtx.sh
+rm /tmp/${STATION}.pid
